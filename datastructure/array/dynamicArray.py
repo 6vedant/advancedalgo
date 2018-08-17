c9b20100fa4cf679e8b3c66842b20fc7a7dd1e33
Vedant Jha <6vedant@gmail.com>
2018-08-17 15:11:31 +0530

    Create dynamicArray.py

---
+++ datastructure/array/dynamicArray.py
@@ -0,0 +1,19 @@
+# created by Vedant
+# https://www.hackerrank.com/challenges/dynamic-array/problem
+
+N, Q = map(int, input().split())
+lastAns = 0
+seqList = [[] for _ in range(N)]
+
+for __ in range(Q):
+	q, x, y = map(int, input().split())
+	index = (x ^ lastAns) % N
+	seq = seqList[index]
+	if q == 1:
+		seq.append(y)
+	elif q == 2:
+		size = len(seq)
+		lastAns = seq[y % size]
+		print(lastAns)
+	else:
+		raise ValueError()
